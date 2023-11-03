59fb1961be0629424c73e3ebe299781d4f071791
Asad <mdasadask@gmail.com>
2023-11-03 12:47:41 +0600

    add last child with query selector and nth

---
+++ script.js
@@ -0,0 +1,12 @@
+// let items = document.querySelector('.item:nth-child(1)');
+// items.style.color = "red";
+
+
+// let elements = document.querySelectorAll('.item:nth-child(1)');
+// for (const element of elements) {
+//     element.style.color = "red";
+// }
+
+// console.dir(document);
+
+
